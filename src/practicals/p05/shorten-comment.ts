9c7ec494ae2b08f32c2e98e0f07efcaeb5470b19
Sunshine <sunshine.17923@gmail.com>
2026-02-06 16:15:20 +0700

    p05-06

--- src/practicals/p05/shorten-comment.ts
+++ src/practicals/p05/shorten-comment.ts
@@ -1,24 +1,39 @@
+// const comment:string  = "TypeScript is a strongly typed programming language"
+
+//  function shortenComment(comment: string): string {
+//   let keep = comment.split(" ")
+//   let temp = ""
+
+//   for(let i = 0;i < keep.length; i ++) {
+//     if(keep[i] === "null") {
+//       return ""
+//     }
+//     if(keep[i].length>= 5 && keep[i].length <= 10) {
+//       temp += keep[i] 
+//       if( i !== keep.length - 1 ) {
+//         temp += " "
+//       }
+//     } 
+//   }
+//   return temp.trim()
+  
+// }
+
+// console.log(shortenComment(comment))
+
+
 const comment:string  = "TypeScript is a strongly typed programming language"
 
 export function shortenComment(comment: string): string {
-  let keep = comment.split(" ")
+  let aaa = comment.split(" ")
   let temp = ""
-
-  for(let i = 0;i < keep.length; i ++) {
-    if(keep[i] === "null") {
+  for(let i = 0; i < aaa.length; i ++) {
+    if(aaa[i] === "null") {
       return ""
     }
-    if(keep[i].length>= 5 && keep[i].length <= 10) {
-      temp += keep[i] 
-      if( i !== keep.length - 1 ) {
-        temp += " "
-      }
-    } else {
-      return ""
-    } 
+    if(aaa[i].length >= 5 && aaa[i].length <= 10) {
+      temp += aaa[i] += " "
+    }
   }
-  return temp
-  
-}
-
-
+  return temp.trim()
+ }
